bf5a61e7c322addcbd2c360a211dad21d15bf94d
Sellentis <jaredbay1000@gmail.com>
2021-06-23 21:31:22 +0300

    db set

--- index.js
+++ index.js
@@ -1,62 +1,56 @@
+const {MongoClient} = require('mongodb')
 const TelegramApi = require('node-telegram-bot-api')
 
 // const local = require('./local.json');
 
 // const token = local.token
 
+const client = new MongoClient('mongodb+srv://Sellentis:sellentis@sellentis-telegram-bot.9bhpv.mongodb.net/Sellentis-tg-bot?retryWrites=true&w=majority')
+
 const token = "1806990129:AAFwG_rKrudokdZl5eQX7QcnlNN3771gQ8w"
 
 const bot = new TelegramApi(token, {polling: true})
 
-const chats = {}
 
-const gameOptions = {
-	reply_markup: JSON.stringify({
-		inline_keyboard: [
-			[{text: '1', callback_data: '1'},{text: '2', callback_data: '2'},{text: '3', callback_data: '3'}],
-			[{text: '4', callback_data: '4'},{text: '5', callback_data: '5'},{text: '6', callback_data: '6'}],
-			[{text: '7', callback_data: '7'},{text: '8', callback_data: '8'},{text: '9', callback_data: '9'}]
-		]
-	})
-}
+const start = async () => {
 
-const start = () => {
 	bot.setMyCommands([
 		{command: '/info', description: 'Информация'},
-		// {command: '/game', description: 'Сыграем?'}
 	])
 
 	bot.on('message', async msg => {
 
 		const text = msg.text;
 		const chatId = msg.chat.id;
+		const username = msg.chat.username
 
 		if (text === '/start') {
-			return bot.sendMessage(chatId, `Привет!`)
+
+			try {
+				await client.connect()
+				console.log("-----------Соединение установлено-----------")
+				const users = client.db().collection('users')
+				const user = await users.findOne({name: msg.chat.username})
+				if (user) {
+
+				}
+				else {
+					await users.insertOne({name: username, id: chatId})
+				}
+			}
+			catch (e) {
+				console.log(e)
+			}
+
+			return bot.sendMessage(chatId, `Привет, ${username}`)
 		}
 		if (text === '/info') {
 			return bot.sendMessage(chatId, `Меня зовут Селлентисс`)
 		}
-		if (text === "/game") {
-			await bot.sendMessage(chatId, `Я загадал цифру от 0 до 9, угадай ее`)
-			const randomNumber = Math.floor(Math.random()*10)
-			chats[chatId] = randomNumber;
-			return bot.sendMessage(chatId, `Дело сделано, отгадывай!`, gameOptions)
-		}
+		console.log()
 		return bot.sendMessage(chatId, `Я тебя не понимаю`)
-		return bot.sendMessage(chatId, `${local}`)
 	})
 
-	bot.on('callback_query', async msg => {
-		const data = msg.data;
-		const chatId = msg.message.chat.id;
-		if (data === chats[chatId]) {
-			return await bot.sendMessage(chatId, `Ты выбрал цифру ${data}. И ты угадал!`)
-		}
-		else {
-			return await bot.sendMessage(chatId, `Ты не угадал =( попробуй еще раз, бот загадал ${chats[chatId]}`)
-		}
-	})
 }
 
 start()
